87bc223b4b8ddb0f9133a30746c6a7db032c7dc7
Naedra <naedra.feliciano@upr.edu>
2026-04-27 19:31:48 -0400

    Basic Calculator Functionality

--- BasicCalculator/src/BasicCalculator/BasicCalculatorFunctionality.java
+++ BasicCalculator/src/BasicCalculator/BasicCalculatorFunctionality.java
@@ -1,6 +1,7 @@
 package BasicCalculator;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 public class BasicCalculatorFunctionality {
 	
@@ -45,5 +46,47 @@ public class BasicCalculatorFunctionality {
 	  
 	  return division;
 	}
+	//Calculation for square Root
+	public double squareRoot(int number) {
+		
+		return  Math.sqrt(number);
+	}
+	// Power option 6
+	public int power( int num,  int power) {
+		int calc =1;
+		for(int i =0; i < power; i++) {
+			calc *= num;
+		}
+		return calc;
+	}
+ //Dot product option 7. This option divides into two sub-options to calculate the dot product
+	// OPTION 1: Calculate the dot product using the angle
+	//OPTION 2: Calculate the dot product by Sum
+	//**NOTE: For now the dot product will only accept a vector with two components
+	public double doTproductSum(int ai,int  ay, int bi, int by) {
+		int dotPod = ai*bi + ay*by;
+		return dotPod;
+	}
 	
-}
+	//OPTION 1 for option 7, Calculate the Dot product using the angle
+	
+	public double dotProductAngle(double angle, int ai, int ay, int bi, int by) {
+		double AMagnitude = Math.sqrt(ai*ai+ ay*ay);
+		double BMagnitude = Math.sqrt(bi*bi + by*by);
+		
+		double dot = AMagnitude * BMagnitude * Math.cos(Math.toRadians(angle));
+		return dot;
+	}
+	
+//Option 8 Factorial using recursion
+	public int factorial(int n) {
+		if (n ==1 || n ==0) {
+			return 1;
+		}
+		return factorial(n-1) * n;
+	}
+	
+		
+	}
+	
+
--- BasicCalculator/src/BasicCalculator/Main.java
+++ BasicCalculator/src/BasicCalculator/Main.java
@@ -10,8 +10,17 @@ public class Main {
 		Functionality convertNums = new Functionality();
 		
 		try (Scanner uinp = new Scanner(System.in)) {//Using try and catch to manage 
+			
+		
 				
 	    System.out.println("Welcome to Basic Calculator! ");
+	    
+	    System.out.println("Please select what type of calculation you want  to make today");
+	    System.out.println("a. Arithmetic calculations");
+	    System.out.println("b. Unit Conversion");
+	    String s = uinp.next();
+	    
+	    if(s.equals("a")) {
 		
 		 System.out.println("Please select the option you want to calculate(Please only write the number)");
 		 System.out.println("Select one of the following \n"
@@ -22,7 +31,7 @@ public class Main {
 				+ "5. Square Root \n"
 				+ "6. Power \n"
 				+ "7. Dot Product \n" +
-				"8. Set \n" + "9. Intersection \n" + "10. Union \n" +"11. Factorial");
+				"8. Factorial");
 		//uinp.next();
 		
 		
@@ -41,8 +50,8 @@ public class Main {
 		uinp.nextLine();
 		BasicCalculatorFunctionality test = new BasicCalculatorFunctionality();
 		
-		//Using the convertInput function to return a Arraylist from the user input
-		//This method is used by varoius mathematic operations
+		//Using the convertInput function to return a ArrayList from the user input
+		//This method is used by various mathematics operations
 		
 		if(selection == 1) {
 			
@@ -90,11 +99,139 @@ public class Main {
 			System.out.println("With a remainder of: " + numerator % denominator);
 		}//End of selection 4
 		
+		if(selection ==5) {
+			System.out.println("Please enter the number: " );
+			int num = uinp.nextInt();
+			double sqrtroot = test.squareRoot(num);
+			
+			System.out.println("The square root of: "+ num + " is: " + sqrtroot);
+		}
+		//End of 5
 		
-	} catch (NumberFormatException e) {
+	 if(selection == 6) {
+		 System.out.println("Please enter the number: ");
+		 int num = uinp.nextInt();
+		 
+		 System.out.println("Please enter the power you want to calculate: ");
+		 int power = uinp.nextInt();
+		 
+		 int pow = test.power(num, power);
+		 
+		 System.out.println("The power of: "+ num + " elevated: " + power + " is: " + pow );
+	 }
+	 //End of 6
+	 
+	 if(selection == 7) {
+		 System.out.println("To calculate the dot product:" );
+		 System.out.println("1. Select number 1, If you have the angle");
+		 System.out.println("2. Select number 2, If you only have the magnitude" );
+		 
+		 
+		 int select = 0;
+		 
+		 //Manage exceptions in case of incorrect input
+		 while(select != 1 && select != 2) {
+		 
+		 try {
+			 select = uinp.nextInt();
+		 }
+		 catch(Exception e){
+			 System.out.println("Invalid input. Please select a valid option 1 or 2");
+			 uinp.nextLine();
+			 
+		 }
+		 
+		 if(select == 1) {
+			 System.out.println("Please enter component i of vector a : ");
+			 int ai = uinp.nextInt();
+			 
+			 System.out.println("Please enter component y of vector a : ");
+			 int ay = uinp.nextInt();
+			 
+			 System.out.println("Please enter component i of  vector b : ");
+			 int bi = uinp.nextInt();
+			 
+			 System.out.println("Please enter component y of vector b : ");
+			 int by = uinp.nextInt();
+			 
+			 double dotcomp = test.doTproductSum(ai, ay, bi, by);
+			 System.out.println("The dot product is: " + dotcomp);
+			 
+		 }
+		 else if (select ==2){
+			 System.out.println("Please enter the value of the angle : ");
+			 int angle = uinp.nextInt();
+			 System.out.println("Please enter component i of vector a : ");
+			 int ai = uinp.nextInt();
+			 
+			 System.out.println("Please enter component y of vector a : ");
+			 int ay = uinp.nextInt();
+			 
+			 System.out.println("Please enter component i of  vector b : ");
+			 int bi = uinp.nextInt();
+			 
+			 System.out.println("Please enter component y of vector b : ");
+			 int by = uinp.nextInt();
+			 
+			 double dotAngle = test.dotProductAngle(angle, ai, ay, bi, by);
+			 
+			 System.out.println("The dot product is: " + dotAngle);
+			 
+		 }
+		 else {
+		 System.out.println("That is not an option. Please select a valid number from the list.");
+		 }
+		 }
+	 }
+	 //End of 7
+	 if (selection == 8) {
+		 System.out.println("Please enter the number: " );
+		 int num = uinp.nextInt();
+		 
+		 int result = test.factorial(num);
+		 
+		 System.out.println("The factorial for: " + num + " is: "+ result );
+		 
+	 }
+	 
+	
+	    
+	} //End of a
+	   else if (s.equals("b")) {
+		   System.out.println("Please select the number of the unit you want to convert: ");
+		   System.out.println("1. Temperature (F, C, K)");
+		   System.out.println("2. Lenght (cm, km, m, etc.)");
+		   System.out.println("3 .Weight (lb, kg )");
+		   int unit = uinp.nextInt();
+		   if(unit == 1) {
+			   System.out.println("Please select the conversion you want to make ");
+			   System.out.println("a. Farenheit to Celcius (F to C) ");
+			   System.out.println("b. Celcius to Farenheit (C to F) ");
+			   System.out.println("c. Farenheit to Kelvin (F to K) ");
+			   System.out.println("d. Celcius to Kelvin (C to K) ");
+			   System.out.println("e. Kelvin to Celcius  (K to C) ");
+			   System.out.println("f. Farenheit to Kelvin (F to K) ");
+			   String temp = uinp.next();
+			   
+			   
+			   if( temp.equals("a")) {
+				   System.out.println("Please enter the Farenheit temperature : ");
+				   double tF = uinp.nextInt();
+				   
+				   
+			   }
+			   
+		   }
+	   } 
+		}// End of if a
+		
+ 
+	
+	
+	    catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	
+			
 }
 }
